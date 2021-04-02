fe9564a80dd0ccd9fbd099e9863b6240ae048a5f
Malachi <malachialusa@gmail.com>
2021-04-02 11:08:21 -0600

    inital files, added Banner component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Banner from './components/Banner';
+
+function App() {
+  return (
+    <div>
+      <Banner>
+      </Banner>
+    </div>
+  );
+}
+
+export default App;
