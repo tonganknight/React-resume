3de409bba1c12ddc9a0e7dc586a0d0756ae81247
Malachi <malachialusa@gmail.com>
2021-04-03 16:11:15 -0600

    Finished basic app

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,30 +2,19 @@ import React from 'react';
 
 function Nav({currentPage, handlepageChange}) {
     
-    const tabs =['About', 'Resume', 'Portfolio', 'Contact']
+    const tabs =['About', 'Resume', 'Projects', 'Contact']
     return(
         <nav className="navbar navbar-light bg-secondary">
-            <div className="flexnav">
+            
                 {tabs.map(tab => (
-                    <div key={tab} className="navtextflex"><a href={'#' + tab.toLowerCase()}
+                    <div key={tab} className="navtext"><a href={'#' + tab.toLowerCase()}
                     onClick={() => handlepageChange(tab)}
-                    className={
-                    currentPage === tab ? 'nav-link active' : 'nav-link'
+                    className={ 
+                    currentPage === tab ? 'nav-link active' : 'nav-link', "navtext"
                     } >{tab}</a></div>
                 ))}
-            {/* /*<div className="aboutmeflex">
-                <a data-testid="about" id="aboutmeclick" className="navtext" href="About">About Me</a>
-            </div>
-            <div className="navtextflex">
-                <a data-testid="resume"  id="resumeclick" className="navtext" href="Resume" >Resume</a>
-            </div>
-            <div className="navtextflex">
-                <a  id="portfolioclick" className="navtext" href="Portfolio">Portfolio</a>
-            </div>
-            <div className="navtextflex">
-                <a  id="Contactclick" className="navtext" href="Contact">Contact</a>
-            </div>  */}
-            </div>
+
+           
         </nav>
     )
 }
