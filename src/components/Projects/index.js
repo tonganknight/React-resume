5d1f8d33a8691a3133ded06df44fa8765c403615
Malachi <malachialusa@gmail.com>
2021-04-06 20:21:33 -0600

    added two more projects

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -18,6 +18,19 @@ function Projects() {
 
         <a className="projectwindow1 windoweffect" target="blank" href="https://tonganknight.github.io/awesome-sauce/"></a>
 
+        <div className="projecttitle">Readviews</div>
+
+        <a className ="repolink" target="blank" href="https://github.com/quaylas/readviews">You can find the repo for Readviews here </a>
+  
+        <a className="windoweffect projectwindow5" target="blank" href="https://readviews-022021.herokuapp.com/"></a>
+
+        <div className="projecttitle">Tech Book</div>
+
+        <a className ="repolink" target="blank" href="https://github.com/tonganknight/Tech-book">You can find the repo for Tech Book  here </a>
+  
+        <a className="windoweffect projectwindow6" target="blank" href="https://the-tech-book.herokuapp.com/"></a>
+
+
         <div className="projecttitle">Taskmaster Pro</div>
 
         <a className ="repolink" target="blank" href="https://github.com/tonganknight/taskmaster-pro">You can find the Git repo for Taskmaster here </a>
@@ -35,8 +48,13 @@ function Projects() {
         <a className ="repolink" target="blank" href="https://github.com/tonganknight/run-buddy">You can find the repo for Run Buddy here </a>
           
         <a className="windoweffect projectwindow4" target="blank" href="https://tonganknight.github.io/run-buddy/"></a>
+
+        
+
         
         </div>
+
+
 
 
            
